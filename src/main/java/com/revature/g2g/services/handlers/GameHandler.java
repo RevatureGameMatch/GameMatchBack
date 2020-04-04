86fcb3d6c17ac4add1dbc776336f783628de9ed4
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-04-04 12:38:11 -0400

    Created ApplicationContextSingleton, GameHandler, PlayerHandler, PlayerRoomJTHandler

---
+++ src/main/java/com/revature/g2g/services/handlers/GameHandler.java
@@ -0,0 +1,48 @@
+package com.revature.g2g.services.handlers;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import com.revature.g2g.models.Game;
+import com.revature.g2g.models.Skill;
+import com.revature.g2g.repositories.GameDAO;
+import com.revature.g2g.repositories.IGameDAO;
+import com.revature.g2g.services.spring.ApplicationContextSingleton;
+
+@Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+public class GameHandler {
+	private IGameDAO repository;
+	public GameHandler() {
+		super();
+		this.repository = (GameDAO) ApplicationContextSingleton.getApplicationContext().getBean("gameDAO");
+	}
+	public GameHandler(IGameDAO repository) {
+		super();
+		this.repository = repository;
+	}
+	public void insert(Game g) {
+		this.repository.insert(g);
+	}
+	public Game findById(int id) {
+		return this.repository.findById(id);
+	}
+	public Game findByName(String name) {
+		return this.repository.findByName(name);
+	}
+	public Set<Game> findAll(){
+		return this.repository.findAll();
+	}
+	public Set<Game> findBySkill(Skill skill){
+		return this.repository.findBySkill(skill);
+	}
+	public void update(Game g) {
+		this.repository.update(g);
+	}
+	public void delete(Game g) {
+		this.repository.delete(g);
+	}
+}

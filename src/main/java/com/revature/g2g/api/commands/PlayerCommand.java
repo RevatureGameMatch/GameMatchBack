5fbb4e80f06cdacbb6def7a2614e11450c09eeee
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-04-04 11:25:19 -0400

    Generated Stubs

---
+++ src/main/java/com/revature/g2g/api/commands/PlayerCommand.java
@@ -0,0 +1,17 @@
+package com.revature.g2g.api.commands;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import com.revature.g2g.api.FrontCommand;
+
+public class PlayerCommand extends FrontCommand{
+
+	@Override
+	public void process() throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

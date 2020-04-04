42d6668bda42137118e15b3eef4e33de16c52294
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-04-04 15:05:31 -0400

    DiscordDriver

---
+++ src/main/java/com/revature/g2g/DiscordDriver.java
@@ -0,0 +1,9 @@
+package com.revature.g2g;
+
+import com.revature.g2g.services.jda.JDASingleton;
+
+public class DiscordDriver {
+	public static void main(String[] args) {
+		JDASingleton.getJda();
+	}
+}

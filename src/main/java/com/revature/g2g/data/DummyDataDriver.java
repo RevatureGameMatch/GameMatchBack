a9d67c08739693a4c946fda787e8743745a16173
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-04-05 21:26:18 -0400

    Final Integrity Check

--- src/main/java/com/revature/g2g/data/DummyDataDriver.java
+++ src/main/java/com/revature/g2g/data/DummyDataDriver.java
@@ -11,13 +11,13 @@ import com.revature.g2g.data.generators.SkillRoomJTGenerator;
 
 public class DummyDataDriver {
 	public static void main(String[] args) {
-//		new PlayerGenerator().generate();
-//		new RoomGenerator().generate();
-//		new PlayerRoomJTGenerator().generate();
-//		new GameGenerator().generate();
-//		new SkillGenerator().generate();
-//		new SkillGameJTGenerator().generate();
+		new PlayerGenerator().generate();
+		new RoomGenerator().generate();
+		new PlayerRoomJTGenerator().generate();
+		new GameGenerator().generate();
+		new SkillGenerator().generate();
+		new SkillGameJTGenerator().generate();
 		new SkillPlayerJTGenerator().generate();
-//		new SkillRoomJTGenerator().generate();
+		new SkillRoomJTGenerator().generate();
 	}
 }

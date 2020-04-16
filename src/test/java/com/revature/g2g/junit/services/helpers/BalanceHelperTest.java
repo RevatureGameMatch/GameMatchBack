a62695aca105f37c3d9612ff8e4b130ab06f9993
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-04-15 21:35:31 -0400

    fixed rounding error by RestController

--- src/test/java/com/revature/g2g/junit/services/helpers/BalanceHelperTest.java
+++ src/test/java/com/revature/g2g/junit/services/helpers/BalanceHelperTest.java
@@ -1,10 +1,12 @@
 package com.revature.g2g.junit.services.helpers;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.revature.g2g.models.SkillPlayerJT;
 import com.revature.g2g.services.helpers.BalanceConstants;
 import com.revature.g2g.services.helpers.BalanceHelper;
 
@@ -39,11 +41,19 @@ public class BalanceHelperTest {
 	@Test
 	public void testValueChangeLow() {
 		float value = -1_000_000f;
-		assertEquals(BalanceConstants.getMaxValue(), balanceHelper.limitValue(value), 0.000001);
+		assertEquals(BalanceConstants.getMinValue(), balanceHelper.limitValue(value), 0.000001);
 	}
 	@Test
 	public void testValueChangeGood() {
 		float value = BalanceConstants.getMaxValue()/2;
 		assertEquals(value, balanceHelper.limitValue(value), 0.000001);
+	}
+	@Test
+	public void testCalculateExpertise() {
+		SkillPlayerJT jt = new SkillPlayerJT();
+		jt.setValue(BalanceConstants.getStartValue());
+		float expertise = balanceHelper.calculateExpertise(jt);
+		assertTrue(expertise > BalanceConstants.getMinExpertise());
+		assertTrue(expertise < BalanceConstants.getMaxExpertise());
 	}
 }

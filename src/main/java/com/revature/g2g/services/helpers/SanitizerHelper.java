2834beb45f97d991112ee7468f78a9999fc2ecf3
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-05-20 16:45:21 -0400

    added comments to explain what types of injection are protected against

--- src/main/java/com/revature/g2g/services/helpers/SanitizerHelper.java
+++ src/main/java/com/revature/g2g/services/helpers/SanitizerHelper.java
@@ -10,8 +10,8 @@ public class SanitizerHelper {
 
 	public static String sanitize(String source) {
 		if (source == null) return null;
-		String result = Jsoup.clean(source, Whitelist.none());
-		result = result.replace(";", "_");
+		String result = Jsoup.clean(source, Whitelist.none());//prevent HTML injection
+		result = result.replace(";", "_");//prevent SQL injection
 		
 		return result;
 	}

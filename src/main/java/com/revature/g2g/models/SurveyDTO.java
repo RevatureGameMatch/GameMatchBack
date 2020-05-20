eac390436f69688a3ad44b5cee61939d1dac2b37
Michael-Kochis <rhishisikk@gmail.com>
2020-05-20 14:38:30 -0500

    Removed unused SurveySkillDTO.

--- src/main/java/com/revature/g2g/models/SurveyDTO.java
+++ src/main/java/com/revature/g2g/models/SurveyDTO.java
@@ -11,11 +11,4 @@ public class SurveyDTO {
 	private List<SkillDTO> skills;
 	private RoomDTO room;
 
-	public SurveyDTO(PlayerDTO player, SurveySkillDTO[] array) {
-		super();
-		this.players.add(player);
-		for (SurveySkillDTO sst: array) {
-			this.skills.add(new SkillDTO(sst.getSkill()) );
-		}
-	}
 }
